f288eb3c2bb631a374a689e5e731ab15e76c6427
Orangehop <bruceyu1@hotmail.com>
2015-12-01 21:12:27 -0500

    fixed some bugs with ui

--- static/index.js
+++ static/index.js
@@ -2,10 +2,73 @@ var signUpButton = function () {
     $("#login").hide();
     $("#cover").hide();
     $("#signup").show();
+    $('#errorMessageSignUp').text("");
 }
 
 var loginButton = function () {
     $("#signup").hide();
     $("#cover").hide();
     $("#login").show();
+    $('#errorMessageLogin').text("");
+}
+
+var login = function () { //logs into application
+    $('#errorMessageLogin').text("");
+    if (!$('#inputEmail').val()) {
+        $('#errorMessageLogin').text("Required fields missing!"); //give error message if fields are missing
+        return;
+    };
+    if (!$('#inputPassword').val()) {
+        $('#errorMessageLogin').text("Required fields missing!"); //give error message if fields are missing
+        return;
+    };
+    var formData = {
+        email: $('#inputEmail').val(),
+        password: $('#inputPassword').val()
+    };
+    $.post('/login', formData).success(function (data, status, xhr) { //sends post request to login
+        getDashboardPage();
+    }).fail(function (data, status, xhr) {
+        $('#errorMessageLogin').text("Authentication Failed!"); // error message if unable to login
+    });
+}
+
+var signUp = function () {
+    $('#errorMessageSignUp').text("");
+    if (!$('#inputEmail').val()) {
+        $('#errorMessageSignUp').text("Required fields missing!"); //give error message if fields are missing
+        return;
+    };
+    if (!$('#inputPassword').val()) {
+        $('#errorMessageSignUp').text("Required fields missing!"); //give error message if fields are missing
+        return;
+    };
+    if (!$('#inputVerifyPassword').val()) {
+        $('#errorMessageSignUp').text("Required fields missing!"); //give error message if fields are missing
+        return;
+    };
+    if (!$('#inputFirstName').val()) {
+        $('#errorMessageSignUp').text("Required fields missing!"); //give error message if fields are missing
+        return;
+    };
+    if (!$('#inputLastName').val()) {
+        $('#errorMessageSignUp').text("Required fields missing!"); //give error message if fields are missing
+        return;
+    };
+    if ($('#inputVerifyPassword').val() !== $('#inputPassword').val()) { //give error message if new password and verify password do not match
+        $('#errorMessageSignUp').text("Passwords do not match!");
+        return;
+    };
+    var formData = {
+        email: $('#inputEmail').val(),
+        password: $('#inputPassword').val(),
+        firstName: $('#inputFirstName').val(),
+        lastName: $('#inputLastName').val()
+    };
+    $.post('/signUp', formData).success(function (data, status, xhr) { //sends post request to sing up
+        currentUser = formData.email;
+        getDashboardPage();
+    }).fail(function (data, status, xhr) {
+        $('#errorMessage').text("Error creating user!"); //error message if user cannot be created
+    });
 }
