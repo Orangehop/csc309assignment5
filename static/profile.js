755f41b6f5275411ebe94c69fb451a77d57e32cb
Liam Jackson <lijax@hotmail.com>
2015-12-04 17:38:00 -0500

    Added editing for profile page

--- static/profile.js
+++ static/profile.js
@@ -1,12 +1,48 @@
 $(document).ready(function(){
 	$("#messageDiv").hide();
-
+	$('#eDescription').hide();
+	$('#eEmail').hide();
+	$('#eLocation').hide();
+	$('#save').hide();
 	$("#contact").click(function(){
 		$(this).hide();
 		$('#messageDiv').show();
+		$('#comment').text(' ');
 	});
 	$("#send").click(function(){
 		$('#messageDiv').hide();
 		$('#contact').show();
+		var message = $('#comment').val();
 	});
+
+	$('#edit').click(function(){
+		$('#Location').hide();
+		$('#Email').hide();
+		$('#Description').hide();
+		$('#eDescription').show();
+		$('#eEmail').show();
+		$('#eLocation').show();
+		$('#edit').hide();
+		$('#save').show();
+		$('#editLocation').val($('#Location').text());
+		$('#editEmail').val($('#Email').text());
+		$('#editDescription').val($('#Description').text());
+	});
+
+	$('#save').click(function(){
+		$('#Location').show();
+		$('#Email').show();
+		$('#Description').show();
+		$('#eDescription').hide();
+		$('#eEmail').hide();
+		$('#eLocation').hide();
+		$('#save').hide();
+		$('#edit').show();
+		$('#Location').text($('#editLocation').val());
+		$('#Email').text($('#editEmail').val());
+		$('#Description').text($('#editDescription').val());
+
+		
+	});
+
 });
